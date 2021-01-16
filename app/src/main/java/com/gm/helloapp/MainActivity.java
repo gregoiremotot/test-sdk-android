5329e2dc8cf6b9f1c62177217a35a6f3ee46bb6f
Grégoire Motot <g.motot@gmail.com>
2021-01-16 12:18:33 +0100

    up

--- app/src/main/java/com/gm/helloapp/MainActivity.java
+++ app/src/main/java/com/gm/helloapp/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.myapplication2;
+package com.gm.helloapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import com.gm.hellolib.PrintHello;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        PrintHello.printMsg();
     }
 }
---
+++ hellolib/src/main/java/com/gm/hellolib/PrintHello.java
@@ -0,0 +1,10 @@
+package com.gm.hellolib;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class PrintHello {
+    public static void printMsg() {
+        System.out.println("Hello from lib");
+    }
+}
